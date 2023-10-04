cb689d6c069bb050c1c29c03098b85bbd2d087e8
sabuj71 <sabujrahman923@gmail.com>
2023-10-04 20:40:27 +0600

    Assignment-3

---
+++ Product.php
@@ -0,0 +1,32 @@
+<?php 
+
+class Product {
+    // TODO: Add properties
+    private $id;
+    private $name;
+    private $price;
+    
+    public function __construct($id, $name, $price) {
+        //  Initialize properties
+        $this->id=$id;
+        $this->name=$name;
+        $this->price=$price;
+    }
+    // Add getFormattedPrice method
+    public function getFormattedPrice(){
+        return 'TK-' .number_format($this->price ,2);
+    }
+    public function showDetails(){
+        echo "Product Details: \n";
+        echo "ID:{$this->id} \n";
+        echo "Name:{$this->name} \n";
+        echo "Price:{$this->getFormattedPrice()} \n";
+    }
+
+}
+
+
+// Test the Product class
+$product = new Product(1, 'T-shirt', 19.99);
+$product->showDetails();
+?>
